449d34164887a5ef3b5c95fb41c236b123a5ca1c
Markus Otto <otto@fs.tum.de>
2017-07-31 15:06:52 +0200

    util: http download

--- util.py
+++ util.py
@@ -1,9 +1,15 @@
 import logging
+import urllib.request
 
 from typing import Union
 
 
 log = logging.getLogger(__name__)
+
+
+def looks_like_url(path: str):
+    url_starts = ["http://", "https://"]
+    return any(path.startswith(x) for x in url_starts)
 
 
 def write_file(path: str, content: str) -> bool:
@@ -24,17 +30,22 @@ def write_file(path: str, content: str) -> bool:
 
 
 def read_file(path: str) -> Union[str, None]:
-    log.info(f'Reading file "{path}".')
-    try:
-        with open(path, "r") as f:
-            content = f.read()
+    if looks_like_url(path):
+        with urllib.request.urlopen(path) as f:
+            content = f.read().decode()
         return content
-    except PermissionError:
-        log.error(f'No permission to open "{path}".')
-        return None
-    except IsADirectoryError:
-        log.error(f'Cannot open directory "{path}".')
-        return None
-    except IOError:
-        log.exception(f'Error reading file "{path}".')
-        return None
+    else:
+        log.info(f'Reading file "{path}".')
+        try:
+            with open(path, "r") as f:
+                content = f.read()
+            return content
+        except PermissionError:
+            log.error(f'No permission to open "{path}".')
+            return None
+        except IsADirectoryError:
+            log.error(f'Cannot open directory "{path}".')
+            return None
+        except IOError:
+            log.exception(f'Error reading file "{path}".')
+            return None
